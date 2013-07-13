22e9ab667665a6725ecaf0f760dd91dc6ac3dd78
Nandu Majeti <nandu.majeti@gmail.com>
2013-07-13 05:48:47 +0000

    updated grader.js for HW3.3

--- grader.js
+++ grader.js
@@ -24,8 +24,19 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var urlContent = function(url){
+     rest.get(url).on('complete', function(result) {
+    if (result instanceof Error) {
+      /// some error handling
+    } else {
+      fs.writeFileSync(HTMLFILE_DEFAULT, result);
+    }
+  });
+}
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,6 +76,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'URL to index.html', clone(urlContent), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
